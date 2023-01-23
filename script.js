f84dbc8df5da3b28e01506575afe559567ecee37
Damion McLoone <damion.mcloone@gmail.com>
2023-01-23 08:29:14 -0600

    Add computer desision logic

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice(){
+    let computerChoice;
+    let roll = Math.floor(Math.random() * 3) + 1;
+
+    if(roll === 1){
+        computerChoice = "ROCK";
+    }
+    else if(roll === 2){
+        computerChoice = "PAPER";
+    }
+    else{
+        computerChoice = "SCISSORS";
+    }
+
+    return computerChoice;
+}
