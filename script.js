8b3a7689699f0bef15e983cdbcac0d1d1567c03d
Damion McLoone <damion.mcloone@gmail.com>
2023-01-23 08:30:56 -0600

    Add individual round logic

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+function playRound(playerChoice, computerChoice){
+    let result;
+
+    if(playerChoice === computerChoice){
+        return result = 'tie';
+    }
+    else if(playerChoice === 'ROCK'){
+        if(computerChoice === 'PAPER'){
+            return result = 'computer'
+        }
+        else{
+            return result = 'player'
+        }
+    }
+    else if(playerChoice === 'PAPER'){
+        if(computerChoice === 'SCISSORS'){
+            return result = 'computer'
+        }
+        else{
+            return result = 'player'
+        }
+    }
+    else if(playerChoice === 'SCISSORS'){
+        if(computerChoice === 'ROCK'){
+            return result = 'computer'
+        }
+        else{
+            return result = 'player'
+        }
+    }
+}
+
 function getComputerChoice(){
     let computerChoice;
     let roll = Math.floor(Math.random() * 3) + 1;
